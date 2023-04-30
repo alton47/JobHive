053d9f8f58e529ad060af0711bda1004266b337c
Allan <altonallan47@gmail.com>
2023-04-30 16:51:24 +0200

    Fetch

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -4,12 +4,16 @@ import { useRouter } from 'expo-router'
 import { COLORS, SIZES } from '../../../constants'
 import PopularJobCard from '../../common/cards/popular/PopularJobCard'
 import styles from './popularjobs.style'
-//import useFetch from '../../../hook/useFetch'
+import useFetch from '../../../hook/useFetch'
 
 const Popularjobs = () => {
   const router = useRouter()
-  const isLoading = false
-  const error = false
+
+  const { data, isLoading, error } = useFetch(
+    'search', {
+      query: 'React developer',
+      num_pages: 1
+    })
   
   return (
     <View style={styles.container}>
