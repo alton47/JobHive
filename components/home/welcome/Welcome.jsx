787bdd74f39bebd2e8c5b7c9752384a755446bfe
Allan <altonallan47@gmail.com>
2023-04-30 14:14:29 +0200

    Update Welcome

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
+import { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
+import { icons, SIZES } from '../../../constants'
 import styles from './welcome.style'
 
 const Welcome = () => {
+  const router = useRouter()
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+         <Text style={styles.userName}>Hello Allan</Text>
+         <Text style={styles.welcomeMessage}>Find your perfect Job!</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput />
+        </View>
+      </View>
     </View>
   )
 }
