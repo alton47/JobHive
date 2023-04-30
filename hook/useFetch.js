860719ef6041241a23559787ffd1356e5ec914f5
Allan <altonallan47@gmail.com>
2023-04-30 16:11:52 +0200

    Create useFetch
    endpoint prompt

---
+++ hook/useFetch.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+};
